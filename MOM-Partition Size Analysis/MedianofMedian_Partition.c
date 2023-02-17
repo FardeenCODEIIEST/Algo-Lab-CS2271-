b88065faa78664059b1001b084ac6564f7a5f046
Sk_Fardeen_Hossain <fardeen786iiestcst@gmail.com>
2023-02-17 18:26:28 +0530

    Made some changes

--- MOM-Partition Size Analysis/MedianofMedian_Partition.c
+++ MOM-Partition Size Analysis/MedianofMedian_Partition.c
@@ -9,7 +9,7 @@ void swap(int *a, int *b)
   *b = t;
 }
 
-void insertion_sort(int *arr, int start, int final)
+void insertion_sort(int arr[], int start, int final)
 {
   for (int i = start; i <= final; i++)
   {
@@ -24,44 +24,41 @@ void insertion_sort(int *arr, int start, int final)
   }
 }
 
-int median(int *arr, int start, int final)
+int median(int arr[], int start, int final)
 {
   insertion_sort(arr, start, final);
   int mid = (start + final) / 2;
   return arr[mid];
 }
 
-int median_of_medians(int *arr, int size, int divide_size)
+int median_of_median(int arr[], int arr_size, int divide_size)
 {
-  if (size < divide_size)
+  if (arr_size < divide_size)
   {
-    return median(arr, 0, size - 1);
+    return median(arr, 0, arr_size - 1);
   }
-  int total_groups = size / divide_size;
-  int ele_int_last = size % divide_size;
+
+  int total_group = arr_size / divide_size;
+  int elements_in_last = arr_size % divide_size;
 
   int next_size;
-  if (ele_int_last == 0)
-  {
-    next_size = total_groups;
-  }
+
+  if (elements_in_last == 0)
+    next_size = total_group;
   else
-  {
-    next_size = total_groups + 1;
-  }
+    next_size = total_group + 1;
+
   int next_arr[next_size];
+
   for (int i = 0; i < next_size; i++)
   {
     if (i == next_size - 1)
-    {
-      next_arr[i] = median(arr, divide_size * i, size - 1);
-    }
+      next_arr[i] = median(arr, divide_size * i, arr_size - 1);
     else
-    {
       next_arr[i] = median(arr, divide_size * i, divide_size * (i + 1) - 1);
-    }
   }
-  return median_of_medians(next_arr, next_size, divide_size);
+
+  return median_of_median(next_arr, next_size, divide_size);
 }
 
 int partition(int *arr, int start, int final)
@@ -70,90 +67,95 @@ int partition(int *arr, int start, int final)
 
   int left = start;
   int right = final;
+
   while (1)
   {
     while (arr[left] < pivot_value)
-    {
       left++;
-    }
-    while (arr[right] > pivot_value)
-    {
+
+    while (pivot_value < arr[right])
       right--;
-    }
+
     if (arr[left] == arr[right])
     {
       if (left == right)
-      {
         return left;
-      }
       else
-      {
-        return right--;
-      }
+        right--;
     }
     else if (left < right)
-    {
       swap(&arr[left], &arr[right]);
-    }
     else
-    {
       return left;
-    }
   }
 }
 
-int partition_size(int *arr, int size, int divide_size)
+int find_partition_size_MoM(int *arr, int arr_size, int divide_size)
 {
-  int median = median_of_medians(arr, size, divide_size);
-  for (int i = 0; i < size; i++)
-  {
-    if (arr[i] == median)
+  int val = median_of_median(arr, arr_size, divide_size);
+
+  for (int i = 0; i < arr_size; i++)
+    if (arr[i] == val)
     {
       swap(&arr[0], &arr[i]);
-      return partition(arr, 0, size - 1);
+      return partition(arr, 0, arr_size - 1);
     }
-  }
 }
 
 void toArray(int *arr, int n)
 {
-  FILE *fin = fopen("normal_distribution.csv", "r");
+
+  FILE *fin = fopen("uniform_distribution.csv", "r");
+
+  int temp = 0;
   for (int i = 0; i < n; i++)
   {
-    int skip = rand() % 15;
-    for (int j = 0; j < skip; j++)
+    int skip_line = rand() % 20;
+
+    for (int j = 0; j < skip_line; j++)
       ;
-    fscanf(fin, "%d\n", &arr[i]);
+
+    fscanf(fin, "%d\n", &temp);
+    arr[i] = (int)temp;
   }
+
   fclose(fin);
 }
 
 int main()
 {
-  const int SIZE = 100000;
   srand(time(0));
 
-  int divide_size = 7;
-  int rounds = 15;
-  int parti_size;
-  float avg_parti_size;
+  const int max_arr_size = 1e5;
 
-  FILE *fp = fopen("MOM_normal_part_7_obs.csv", "w");
-  fprintf(fp, "Size,Avg Partition Size\n");
-  for (int sz = 100; sz <= SIZE; sz += 100)
+  int divide_size = 7;
+  int rounds = 10;
+  int part_size;
+  float avg_part_size;
+
+  FILE *fout = fopen("MoM_uniform_part_7_obs.csv", "w");
+  fprintf(fout, "Size,Avg Partition Size\n");
+
+  for (int arr_size = 100; arr_size <= max_arr_size; arr_size += 100)
   {
-    int arr[sz];
-    avg_parti_size = 0;
+    int arr[arr_size];
+    avg_part_size = 0;
+
     for (int i = 0; i < rounds; i++)
     {
-      toArray(arr, sz);
-      parti_size = partition_size(arr, sz, divide_size);
-      avg_parti_size += parti_size;
+      toArray(arr, arr_size);
+
+      part_size = find_partition_size_MoM(arr, arr_size, divide_size);
+
+      avg_part_size += part_size;
     }
-    avg_parti_size /= rounds;
-    printf("Size: %d Avg Partition Size: %f\n", sz, avg_parti_size);
-    fprintf(fp, "%d,%f\n", sz, avg_parti_size);
+
+    avg_part_size = avg_part_size / rounds;
+
+    printf("Size: %d Avg Part Size: %f \n", arr_size, avg_part_size);
+    fprintf(fout, "%d,%f\n", arr_size, avg_part_size);
   }
-  fclose(fp);
+
+  fclose(fout);
   return 0;
 }
